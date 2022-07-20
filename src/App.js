f56dc30a39a68fa034fa5b233c8bf8f8d8f82127
Angel Ortiz <107958827+alortizOQ@users.noreply.github.com>
2022-07-20 02:37:29 -0500

    Add files via upload
    Here I added the skills part of my Portfolio

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <About />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
