173c5d769c8a6f1f657aff6156e7f1a0d7e06ab5
Angel Ortiz <107958827+alortizOQ@users.noreply.github.com>
2022-07-19 23:40:43 -0500

    Add files via upload
    Here I made the about section of the website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import About from "./components/About";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <About />
+    </div>
+  );
+}
+
+export default App;
