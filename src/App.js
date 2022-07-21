681ac01f7e34ff22e539ec4cd193569aa14c63c5
Angel Ortiz <107958827+alortizOQ@users.noreply.github.com>
2022-07-20 20:25:11 -0500

    Add files via upload
    Here I added my BZBT work which was a failed experiment with my friends Victor and Daniel Gonzalez

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <About />
+      <Skills />
+      <Work />
+    </div>
+  );
+}
+
+export default App;
